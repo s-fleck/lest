cf44637cad68b656e6fd498a1f108f2908525f58
Stefan Fleck <stefan.b.fleck@gmail.com>
2018-08-12 21:59:09 +0200

    explicitely handle n = 0

--- src/cumall_impl.c
+++ src/cumall_impl.c
@@ -4,27 +4,26 @@
 
 SEXP cumall_impl(SEXP x) {
   SEXP res = PROTECT(allocVector(LGLSXP, XLENGTH(x)));
-
-  int *p_x = LOGICAL(x);
-  int *p_res = LOGICAL(res);
   R_xlen_t n = XLENGTH(x);
 
   if (n == 0){
     UNPROTECT(1);
     return(res);
+  } else {
+    memset(LOGICAL(res), 0, n * sizeof(int));
   }
 
-  memset(LOGICAL(res), 0, n * sizeof(int));
+  int *p_x = LOGICAL(x);
+  int *p_res = LOGICAL(res);
   p_res[0] = p_x[0];
 
   for (R_xlen_t i = 1; i < n; i++) {
-    if (p_x[i] == 1){
+    if (p_x[i] == TRUE)
       p_res[i] = p_res[i - 1];
-    } else if (p_x[i] == NA_LOGICAL){
+    else if (p_x[i] == NA_LOGICAL)
       p_res[i] = NA_LOGICAL;
-    } else {
+    else
       break;
-    }
   }
 
   UNPROTECT(1);
